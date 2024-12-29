5a00460273706a73143dfac04703f50477882e4a
Daniel Gorbe <daniel@gorbe.io>
2024-12-29 20:37:33 +0100

    Create SitemapIsIndex() function

--- sitemap.go
+++ sitemap.go
@@ -1,7 +1,13 @@
 package sitemap
 
+import "bytes"
+
 const XMLNameSpace = "http://www.sitemaps.org/schemas/sitemap/0.9"
 
 type Sitemap interface {
 	ToXML() ([]byte, error)
+}
+
+func SitemapIsIndex(data []byte) bool {
+	return bytes.Contains(data, []byte("sitemapindex"))
 }
