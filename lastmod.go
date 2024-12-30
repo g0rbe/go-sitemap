9b12ec085b02412ce86d0868551266f77eb8c653
Daniel Gorbe <daniel@gorbe.io>
2024-12-30 14:30:58 +0100

    Add Time() method to LastModification

--- lastmod.go
+++ lastmod.go
@@ -1,6 +1,9 @@
 package sitemap
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+	"time"
+)
 
 // The date of last modification of the page. This date should be in W3C Datetime format. This format allows you to omit the time portion, if desired, and use YYYY-MM-DD.
 //
@@ -26,6 +29,30 @@ func (l *LastModification) String() string {
 	return string(*l)
 }
 
+func (l *LastModification) Time() (time.Time, error) {
+
+	var layout string
+
+	switch len(*l) {
+	case 4:
+		layout = "2006"
+	case 7:
+		layout = "2006-01"
+	case 10:
+		layout = "2006-01-02"
+	case 20:
+		layout = "2006-01-02T15:04:05Z"
+	case 24:
+		layout = "2006-01-02T15:04:05.999Z"
+	case 25:
+		layout = "2006-01-02T15:04:05.999999999Z07:00"
+	default:
+		layout = time.RFC3339Nano
+	}
+
+	return time.Parse(layout, l.String())
+
+}
 func (l *LastModification) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
 
 	// CHange the start and end tag to "lastmod"
